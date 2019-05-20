58502fb9b5bef776670f784a9b4c5fad9ebefc9e
Ruwai <chrisl6uie@gmail.com>
2019-05-20 12:44:54 -0700

    tested more methods, making a preprocessing script that handles API requests from alphavantage, and makes the data ready for the fbprophet model

---
+++ preprocess.py
@@ -0,0 +1,9 @@
+import fbprophet
+import pandas as pd 
+import numpy as np 
+import pytrends
+import pandas_datareader.data as web
+from alpha_vantage.timeseries import TimeSeries 
+
+class Preprocess():
+	
