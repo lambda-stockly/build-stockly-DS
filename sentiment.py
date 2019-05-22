7421b01bc82264d7bee620551057cfa45a57d248
Ruwai <chrisl6uie@gmail.com>
2019-05-22 01:57:41 -0700

    transfer

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,19 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from alpha_vantage.timeseries import TimeSeries
 import os
 import fbprophet
 from sklearn.model_selection import train_test_splits
+import tweepy
+import re
+import basilica
+import gensim
+import gensim.models.doc2vec as doc2vec
+from gensim.models.doc2vec import TaggedDocument
+from gensim.models import Doc2Vec
+
+'''
+    This script will be for Reddit Sentiment and Twitter Sentiment.
+
+'''
+
